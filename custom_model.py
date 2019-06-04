c3ba8a1d3f0141b799d2bff0e78b4fc71adadb85
jak-ing <lupin2702@gmail.com>
2019-06-04 17:20:57 +0200

    Add pooling for group norm ResNet

--- custom_model.py
+++ custom_model.py
@@ -1,4 +1,5 @@
 from chainer import functions as F
+from chainer.functions.pooling.average_pooling_2d import average_pooling_2d
 from chainer import links as L
 from chainer import Chain
 from resnet_group_norm import ResNet as ResNetGroupNorm
@@ -8,7 +9,7 @@ class CustomModel(Chain):
         super(CustomModel, self).__init__()
         with self.init_scope():
             self.resNet=ResNetGroupNorm(n_layers=18)
-            self.l1=L.Linear(2138, 1024)
+            self.l1=L.Linear(602, 1024)
             self.l2=L.Linear(1024, 1024)
             self.l3=L.Linear(1024, n_actions)
 
@@ -16,8 +17,15 @@ class CustomModel(Chain):
         image, history = x[0], x[1]
         image = F.reshape(image, (-1,3,224,224))
         history = F.reshape(history.astype('float32'),(-1,90))
-        h1 = F.relu(self.resNet(image, layers=['pool5'])['pool5'])
-        h1 = F.reshape(F.concat((h1, history), axis=1), (-1,2138))
+        h1 = self.resNet(image)
+
+        # pooling as done here: https://github.com/chainer/chainer/blob/v6.0.0/chainer/links/model/vision/resnet.py#L655
+        n, channel, rows, cols = h1.shape
+        h1 = average_pooling_2d(h1, (rows, cols), stride=1)
+        h1 = F.reshape(h1, (n, channel))
+
+        h1 = F.relu(h1)
+        h1 = F.reshape(F.concat((h1, history), axis=1), (-1,602))
         h2 = F.relu(self.l1(h1))
         h3 = F.relu(self.l2(h2))
         return F.relu(self.l3(h3))
