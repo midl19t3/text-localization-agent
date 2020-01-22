698e2649b975f317f8fd110bef2817ecc26fd3a1
Erik Ziegler <erik.ziegler@student.hpi.uni-potsdam.de>
2020-01-22 18:37:07 +0100

    Add support for synthtext dataset

--- datasets.py
+++ datasets.py
@@ -1,13 +1,13 @@
 import json
 import os
 import numpy as np
+import scipy.io as sio
 
 class Dataset:
     id = 'noop'
 
-    def __init__(self, config):
-        config_prefix = f"{self.id}_dataset_"
-        self.config = {k.replace(config_prefix, ''): v for k, v in config.items() if config_prefix in k}
+    def __init__(self, dataset_path):
+        self.dataset_path = os.path.abspath(dataset_path)
 
     def data(self):
         pass  
@@ -16,13 +16,12 @@ class SimpleDataset(Dataset):
     id = 'simple'
 
     def data(self):
-        image_locations_file_path = self.config['image_locations']
-        dataset_base_path = os.path.dirname(image_locations_file_path)
+        image_locations_file = os.path.join(self.dataset_path, 'image_locations.txt')
+        relative_image_paths = np.loadtxt(image_locations_file, dtype=str)
+        absolute_image_paths = [os.path.join(self.dataset_path, image_path) for image_path in relative_image_paths]
         
-        relative_image_paths = np.loadtxt(image_locations_file_path, dtype=str)
-        absolute_image_paths = [dataset_base_path + i.strip('.') for i in relative_image_paths]
-        
-        bounding_boxes = np.load(self.config['bounding_boxes'], allow_pickle=True)
+        bounding_boxes_file = os.path.join(self.dataset_path, 'bounding_boxes.npy')
+        bounding_boxes = np.load(bounding_boxes_file, allow_pickle=True)
 
         return absolute_image_paths, bounding_boxes
 
@@ -30,8 +29,7 @@ class SignDataset(Dataset):
     id = 'sign'
 
     def data(self):
-        config_file_path = self.config['config']
-        dataset_base_path = os.path.dirname(config_file_path)
+        config_file_path = os.path.join(self.dataset_path, 'training.json')
 
         absolute_image_paths = []
         bounding_boxes = []
@@ -39,19 +37,25 @@ class SignDataset(Dataset):
         with open(config_file_path) as f:
             data = json.load(f)
             for image in data:
-                absolute_path = os.path.join(dataset_base_path, image['file_name'])
+                absolute_path = os.path.join(self.dataset_path, image['file_name'])
                 if not os.path.exists(absolute_path): continue
                 absolute_paths.append(absolute_path)
                 bboxes.append(list(image['bounding_boxes']))
 
         return absolute_image_paths, bounding_boxes
 
-class SynthDataset(Dataset):
-    id = 'synth'
+class SynthTextDataset(Dataset):
+    id = 'synthtext'
 
     def data(self):
-        pass
-
+        mat_file = os.path.join(self.dataset_path, 'gt.mat')
+        mat = sio.loadmat(mat_file)
+        relative_image_paths = [arr[0] for arr in mat['imnames'][0]]
+        absolute_image_paths = [os.path.join(self.dataset_path, image_path) for image_path in relative_image_paths]
+        bounding_boxes = mat['wordBB'][0]
+        return absolute_image_paths, bounding_boxes
+        
 def get_dataset(id):
-    datasets = [SimpleDataset, SignDataset, SynthDataset]
+    datasets = [SimpleDataset, SignDataset, SynthTextDataset]
     return {Dataset.id: Dataset for Dataset in datasets}[id]
+  
